405e12ec8ef6d46ae0232ad37a2189e41c5dca26
Muhammad Huzaifa <huzaifa4302208@cloud.neduet.edu.pk>
2023-09-24 11:03:36 +0500

    Second Commit

---
+++ src/Component/Basics/Thanks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+const Thanks = () => {
+  return (
+    <div className="welcome-screen">
+      <p className="welcome-text">Thanks for visiting!</p>
+    </div>
+  );
+};
+
+export default Thanks;
---
+++ src/Component/Basics/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+const Welcome = () => {
+  return (
+    <div className="welcome-screen">
+      <p className="welcome-text">Welcome to our restaurant!</p>
+    </div>
+  );
+};
+
+export default Welcome;
